5b7f4122242bb68e5d78d786d1d1965f21ba30bd
Ryo Ando <sasigume@gmail.com>
2021-04-29 12:43:30 +0900

    Updated url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
-export const SITE_NAME = 'ナポアンのマイクラ';
-export const SITE_URL = 'napoan.com';
+export const SITE_NAME = 'ナポアンのマイクラ NEXT';
+export const SITE_URL = 'next.napoan.com';
 export const CREATOR_ID = 'sasigume';
-export const SITE_DESC = '日本最速のマインクラフト情報サイト';
+export const SITE_DESC = '日本最大の個人運営マインクラフト情報サイト 超高速バージョン';
 export const SITE_FULL_URL =
   process.env.HTTPS_URL ?? 'https://' + process.env.VERCEL_URL ?? 'https://next.napoan.com';
 
