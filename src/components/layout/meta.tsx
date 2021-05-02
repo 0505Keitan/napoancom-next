6699f246aa803e017e286aac59b244963884a53f
Ryo Ando <sasigume@gmail.com>
2021-05-03 01:53:25 +0900

    added json from wordpress

--- src/components/layout/meta.tsx
+++ src/components/layout/meta.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
-import { SITE_NAME } from '@/lib/constants';
-import { JsonLdStep, Post } from '@/models/contentful/Post';
+import { SITE_DESC, SITE_NAME } from '@/lib/constants';
+import { Post } from '@/models/contentful/Post';
 interface Props {
   desc: string;
   title: string;
@@ -8,8 +8,17 @@ interface Props {
   post?: Post;
 }
 
+interface JsonLdStep {
+  '@type': 'HowToStep';
+  name: string;
+  url: string;
+  text: string;
+}
+
+interface PostSchema {}
+
 // https://dev.to/aviaryan/rendering-json-ld-data-in-nextjs-and-reactjs-29op
-const makePostSchema = (post: Post) => {
+const makePostSchema = (post: Post): PostSchema => {
   let sum = post.like + post.dislike;
   let ratio: number;
   // 擬似的に五段階評価っぽくする
@@ -35,8 +44,10 @@ const makePostSchema = (post: Post) => {
   if (post.headings) {
     steps = post.headings.map((h) => {
       return {
-        // この「h」の中身(text/name)はAPIが作ってくれる
-        ...h,
+        name: h.name,
+        text: h.text,
+        // 一応アンカーリンク
+        url: `${process.env.HTTPS_URL}/${post.slug}#${h.name.replace(` `, `-`)}`,
         '@type': 'HowToStep',
       };
     });
@@ -64,6 +75,125 @@ const makePostSchema = (post: Post) => {
     bestRating: '5',
     worstRating: '0',
     ratingCount: sum ?? 1,
+    '@graph': [
+      {
+        '@type': 'WebSite',
+        '@id': `${process.env.HTTPS_URL}/#website`,
+        url: process.env.HTTPS_URL,
+        name: SITE_NAME,
+        description: SITE_DESC,
+        publisher: {
+          '@id': `${process.env.HTTPS_URL}/#organization`,
+        },
+      },
+      {
+        '@type': 'Organization',
+        '@id': `${process.env.HTTPS_URL}/#organization`,
+        name: 'アエルヨネ',
+        url: 'https://aely.one/',
+      },
+      {
+        '@type': 'BreadcrumbList',
+        '@id': `${process.env.HTTPS_URL}/${post.slug}#breadcrumblist`,
+        itemListElement: [
+          {
+            '@type': 'ListItem',
+            '@id': `${process.env.HTTPS_URL}/#listItem`,
+            position: '1',
+            item: {
+              '@id': `${process.env.HTTPS_URL}/#item`,
+              name: 'ホーム',
+              description: SITE_DESC,
+              url: process.env.HTTPS_URL,
+            },
+            nextItem: `${process.env.HTTPS_URL}/${post.slug}/#listItem`,
+          },
+          {
+            '@type': 'ListItem',
+            '@id': `${process.env.HTTPS_URL}/${post.slug}/#listItem`,
+            position: '2',
+            item: {
+              '@id': `${process.env.HTTPS_URL}/${post.slug}/#item`,
+              name: post.title,
+              description: post.description ?? post.title,
+              url: `${process.env.HTTPS_URL}/${post.slug}/`,
+            },
+            previousItem: `${process.env.HTTPS_URL}/#listItem`,
+          },
+        ],
+      },
+      {
+        '@type': 'Person',
+        '@id': `${process.env.HTTPS_URL}/persons/${post.person?.slug}#author`,
+        url: `${process.env.HTTPS_URL}/persons/${post.person?.slug}`,
+        name: post.person?.displayName,
+        image: {
+          '@type': 'ImageObject',
+          '@id': `${post.person?.picture.url}/#authorImage`,
+          url: post.person?.picture.url,
+          width: '96',
+          height: '96',
+          caption: post.person?.displayName,
+        },
+        sameAs: [post.person?.displayName],
+      },
+      {
+        '@type': 'WebPage',
+        '@id': `${process.env.HTTPS_URL}/${post.slug}/#webpage`,
+        url: `${process.env.HTTPS_URL}/${post.slug}/`,
+        name: post.title,
+        description: post.description ?? post.title,
+        inLanguage: 'ja',
+        isPartOf: {
+          '@id': `${process.env.HTTPS_URL}/#website`,
+        },
+        breadcrumb: {
+          '@id': `${process.env.HTTPS_URL}/${post.slug}/#webpage`,
+        },
+        author: `${process.env.HTTPS_URL}/persons/${post.person?.slug}#author`,
+        creator: `${process.env.HTTPS_URL}/persons/${post.person?.slug}#author`,
+        image: {
+          '@type': 'ImageObject',
+          '@id': `${process.env.HTTPS_URL}/#mainImage`,
+          url: post.heroImage != undefined ? post.heroImage.url : defaultOgp,
+        },
+        primaryImageOfPage: {
+          '@id': `${process.env.HTTPS_URL}/${post.slug}/#mainImage`,
+        },
+        datePublished: post.sys.firstPublishedAt,
+        dateModified: post.sys.publishedAt,
+      },
+      {
+        '@type': 'BlogPosting',
+        '@id': `${process.env.HTTPS_URL}/${post.slug}/#blogposting`,
+        name: post.title,
+        description: post.description ?? post.title,
+        headline: post.title,
+        author: {
+          '@id': `${process.env.HTTPS_URL}/persons/${post.person?.slug}#author`,
+        },
+        publisher: {
+          '@id': `${process.env.HTTPS_URL}/#organization`,
+        },
+        datePublished: post.sys.firstPublishedAt,
+        dateModified: post.sys.publishedAt,
+        articleSection:
+          post.platformsCollection != undefined
+            ? post.platformsCollection.items[0].displayName
+            : '',
+        mainEntityOfPage: {
+          '@id': `${process.env.HTTPS_URL}/${post.slug}/#webpage`,
+        },
+        isPartOf: {
+          '@id': `${process.env.HTTPS_URL}/${post.slug}/#webpage`,
+        },
+        image: {
+          '@type': 'ImageObject',
+          '@id': `${process.env.HTTPS_URL}/${post.slug}/#articleImage`,
+          url: post.heroImage != undefined ? post.heroImage.url : defaultOgp,
+        },
+      },
+    ],
   };
 };
 
